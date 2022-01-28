288e5d73648d82e55fe3413c2dbdee73047ab493
Terenty Rezman <terenty.rezman@mail.ru>
2022-01-28 21:07:02 +0300

    basic layout

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use iced::{
-    text_input, Align, Column, Command, Container, Length, Row, Sandbox, Settings, Space, Text,
-    TextInput, Rule
+    tooltip, button, text_input, 
+    Align, Background, Color, Column, Container, Length, Row, Rule, Sandbox,
+    Settings, Space, Text, TextInput, Vector,
 };
 
-const LIGHT_GRAY: [f32; 3] = [0.5, 0.5, 0.5];
+const LIGHT_GRAY: [f32; 3] = [0.1, 0.1, 0.1];
 
 #[derive(Debug, Clone)]
 enum Message {
@@ -11,6 +12,7 @@ enum Message {
     RodDiameterChanged(String),
     AmplitudeChanged(String),
     FrequencyChanged(String),
+    CalcButtonPressed,
     DoNothing,
 }
 
@@ -23,7 +25,7 @@ struct MiniApp {
 
     amplitude: String,
     amplitude_widget_state: text_input::State,
-    
+
     frequency: String,
     frequency_widget_state: text_input::State,
 
@@ -32,6 +34,8 @@ struct MiniApp {
 
     secondary_result: String,
     secondary_result_widget_state: text_input::State,
+
+    calc_button_state: button::State,
 }
 
 impl Sandbox for MiniApp {
@@ -51,11 +55,13 @@ impl Sandbox for MiniApp {
             frequency: Default::default(),
             frequency_widget_state: Default::default(),
 
-            main_result: Default::default(),
+            main_result: "123".to_string(),
             main_result_widget_state: Default::default(),
 
-            secondary_result: Default::default(),
+            secondary_result: "123".to_string(),
             secondary_result_widget_state: Default::default(),
+
+            calc_button_state: Default::default(),
         }
     }
 
@@ -69,6 +75,7 @@ impl Sandbox for MiniApp {
             Message::RodDiameterChanged(s) => self.rod_diameter = s,
             Message::AmplitudeChanged(s) => self.amplitude = s,
             Message::FrequencyChanged(s) => self.frequency = s,
+            Message::CalcButtonPressed => (),
             Message::DoNothing => (),
         }
     }
@@ -78,7 +85,6 @@ impl Sandbox for MiniApp {
             &mut self.piston_widget_state,
             "",
             &self.piston_diameter,
-            // use as output only
             Message::PistonDiameterChanged,
         )
         .padding(5)
@@ -88,7 +94,6 @@ impl Sandbox for MiniApp {
             &mut self.rod_widget_state,
             "",
             &self.rod_diameter,
-            // use as output only
             Message::RodDiameterChanged,
         )
         .padding(5)
@@ -98,7 +103,6 @@ impl Sandbox for MiniApp {
             &mut self.amplitude_widget_state,
             "",
             &self.amplitude,
-            // use as output only
             Message::AmplitudeChanged,
         )
         .padding(5)
@@ -108,16 +112,26 @@ impl Sandbox for MiniApp {
             &mut self.frequency_widget_state,
             "",
             &self.frequency,
-            // use as output only
             Message::FrequencyChanged,
         )
         .padding(5)
         .width(Length::Units(100));
 
+        let calc_button_widget =
+            button::Button::new(&mut self.calc_button_state, Text::new("Вычислить"))
+                .padding(10)
+                .style(MyButtonStyle)
+                .on_press(Message::CalcButtonPressed);
+
         let input_layout = Column::new()
             .spacing(10)
             .align_items(Align::End)
-            .push(Text::new("Исходные данные"))
+            .push(
+                Row::new()
+                .spacing(10)
+                .push(Text::new("Исходные данные").color(Color::from_rgb8(188, 195, 206)))
+                .push(Space::new(Length::Units(10), Length::Units(1)))
+            )
             .push(Space::new(Length::Units(1), Length::Units(5)))
             .push(
                 Row::new()
@@ -125,7 +139,7 @@ impl Sandbox for MiniApp {
                     .align_items(Align::Center)
                     .push(Text::new("Диаметр поршня:").color(LIGHT_GRAY))
                     .push(piston_widget)
-                    .push(Text::new("мм").color(LIGHT_GRAY).width(Length::Units(30)))
+                    .push(Text::new("мм").color(LIGHT_GRAY).width(Length::Units(30))),
             )
             .push(
                 Row::new()
@@ -133,7 +147,7 @@ impl Sandbox for MiniApp {
                     .align_items(Align::Center)
                     .push(Text::new("Диаметр штока:").color(LIGHT_GRAY))
                     .push(rod_widget)
-                    .push(Text::new("мм").color(LIGHT_GRAY).width(Length::Units(30)))
+                    .push(Text::new("мм").color(LIGHT_GRAY).width(Length::Units(30))),
             )
             .push(Space::new(Length::Units(1), Length::Units(20)))
             .push(
@@ -142,7 +156,7 @@ impl Sandbox for MiniApp {
                     .align_items(Align::Center)
                     .push(Text::new("Амплитуда сигнала:").color(LIGHT_GRAY))
                     .push(amplitude_widget)
-                    .push(Text::new("мм").color(LIGHT_GRAY).width(Length::Units(30)))
+                    .push(Text::new("мм").color(LIGHT_GRAY).width(Length::Units(30))),
             )
             .push(
                 Row::new()
@@ -150,7 +164,15 @@ impl Sandbox for MiniApp {
                     .align_items(Align::Center)
                     .push(Text::new("Частота сигнала:").color(LIGHT_GRAY))
                     .push(frequency_widget)
-                    .push(Text::new("Гц").color(LIGHT_GRAY).width(Length::Units(30)))
+                    .push(Text::new("Гц").color(LIGHT_GRAY).width(Length::Units(30))),
+            )
+            .push(Space::new(Length::Units(1), Length::Units(20)))
+            .push(
+                Row::new()
+                    .spacing(10)
+                    .align_items(Align::Center)
+                    .push(calc_button_widget)
+                    .push(Space::new(Length::Units(30), Length::Units(1))),
             );
 
         let main_result_widget = TextInput::new(
@@ -161,7 +183,8 @@ impl Sandbox for MiniApp {
             |_s| Message::DoNothing,
         )
         .size(30)
-        .padding(10)
+        .padding(0)
+        .style(MyTextInputStyle)
         .width(Length::Units(200));
 
         let secondary_result_widget = TextInput::new(
@@ -171,14 +194,16 @@ impl Sandbox for MiniApp {
             // use as output only
             |_s| Message::DoNothing,
         )
-        .padding(5)
+        .size(30)
+        .padding(0)
+        .style(MyTextInputStyle)
         .width(Length::Units(200));
 
         let result_layout = Column::new()
             .spacing(10)
             .align_items(Align::Start)
-            .push(Text::new("Результат"))
-            .push(Space::new(Length::Units(1), Length::Units(5)))
+            // .push(Text::new("Результат"))
+            // .push(Space::new(Length::Units(1), Length::Units(5)))
             .push(Text::new("Полученный расход Q[л/мин]:").color(LIGHT_GRAY))
             .push(main_result_widget)
             .push(Space::new(Length::Units(1), Length::Units(20)))
@@ -195,13 +220,75 @@ impl Sandbox for MiniApp {
             .height(Length::Units(500));
 
         Container::new(content)
-            .width(Length::Fill) 
+            .width(Length::Fill)
             .height(Length::Fill)
             .center_x()
             .center_y()
             .into()
     }
 }
+
+struct MyButtonStyle;
+
+impl button::StyleSheet for MyButtonStyle {
+    fn active(&self) -> button::Style {
+        button::Style {
+            background: Some(Background::Color(Color::from_rgb8(87, 85, 217))),
+            shadow_offset: Vector::new(0.0, 0.0),
+            text_color: Color::WHITE,
+            border_radius: 3.0,
+            border_width: 0.0,
+            border_color: [0.7, 0.7, 0.7].into(),
+        }
+    }
+
+    fn hovered(&self) -> button::Style {
+        button::Style {
+            background: Some(Background::Color(Color::from_rgb8(75, 72, 214))),
+            ..self.active()
+        }
+    }
+
+    fn pressed(&self) -> button::Style {
+        button::Style {
+            background: Some(Background::Color(Color::from_rgb8(58, 56, 210))),
+            ..self.active()
+        }
+    }
+}
+
+struct MyTextInputStyle;
+
+impl text_input::StyleSheet for MyTextInputStyle {
+    fn active(&self) -> text_input::Style {
+        text_input::Style {
+            background: Background::Color(Color::WHITE),
+            border_radius: 0.0,
+            border_width: 0.0,
+            border_color: Color::from_rgb(0.7, 0.7, 0.7),
+        }
+    }
+
+    fn focused(&self) -> text_input::Style {
+        text_input::Style {
+            border_color: Color::from_rgb(0.5, 0.5, 0.5),
+            ..self.active()
+        }
+    }
+
+    fn placeholder_color(&self) -> Color {
+        Color::from_rgb(0.7, 0.7, 0.7)
+    }
+
+    fn value_color(&self) -> Color {
+        Color::from_rgb8(87, 85, 217)
+    }
+
+    fn selection_color(&self) -> Color {
+        Color::from_rgb(0.8, 0.8, 1.0)
+    }
+}
+
 
 fn main() {
     MiniApp::run(Settings::default());

9d1c5df3c8100ee186ca3c70e49d983ff07c39b2
Terenty Rezman <terenty.rezman@mail.ru>
2022-01-30 00:37:29 +0300

    added calculation logic

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
+use std::f32::consts::PI;
+
 use iced::{
-    tooltip, button, text_input, 
-    Align, Background, Color, Column, Container, Length, Row, Rule, Sandbox,
-    Settings, Space, Text, TextInput, Vector,
+    button, text_input, Align, Background, Color, Column, Container, Font, Length,
+    Row, Rule, Sandbox, Settings, Space, Text, TextInput, Vector,
 };
 
 const LIGHT_GRAY: [f32; 3] = [0.1, 0.1, 0.1];
+
+/// Основные вычисления
+fn try_calc_q(dp: &str, ds: &str, A: &str, freq: &str) -> Result<(f32, f32), &'static str> {
+    let dp: u32 = dp.parse().or(Err("Диаметр поршня: неверное значение"))?;
+    let ds: u32 = ds.parse().or(Err("Диаметр штока: неверное значение"))?;
+    let a: u32 = A.parse().or(Err("Амплитуда сигнала: неверное значение"))?;
+    let freq: f32 = freq.parse().or(Err("Частота сигнала: неверное значение"))?;
+
+    // Перевод в систему СИ:
+    let a_meters = a as f32 / 1000.0; // амплитуда: мм -> м
+    let freq = 2.0 * PI * freq; // частота: Гц -> рад/c
+    let dp_meters = dp as f32 / 1000.0; // площадь поршня: мм -> м
+    let ds_meters = ds as f32 / 1000.0; // площадь штока: мм -> м
+
+    // Площадь рабочей поверхности:
+    let s_work_area = (PI / 4.0) * (dp_meters * dp_meters - ds_meters * ds_meters); // в м^2
+
+    // Основная формула:
+    let q_m3s = freq * a_meters * s_work_area; // расход в ед. СИ
+    let q_litrmin = q_m3s * (1000.0 * 60.0); // расход в л/мин
+
+    Ok((q_litrmin, q_m3s))
+}
 
 #[derive(Debug, Clone)]
 enum Message {
@@ -36,6 +60,8 @@ struct MiniApp {
     secondary_result_widget_state: text_input::State,
 
     calc_button_state: button::State,
+
+    last_error: String,
 }
 
 impl Sandbox for MiniApp {
@@ -55,27 +81,66 @@ impl Sandbox for MiniApp {
             frequency: Default::default(),
             frequency_widget_state: Default::default(),
 
-            main_result: "123".to_string(),
+            main_result: Default::default(),
             main_result_widget_state: Default::default(),
 
-            secondary_result: "123".to_string(),
+            secondary_result: Default::default(),
             secondary_result_widget_state: Default::default(),
 
             calc_button_state: Default::default(),
+
+            last_error: " ".to_string(), // workaround to not hide error_widget on gui
         }
     }
 
     fn title(&self) -> String {
-        "Mini App".to_string()
+        "Q app".to_string()
     }
 
     fn update(&mut self, message: Message) {
         match message {
-            Message::PistonDiameterChanged(s) => self.piston_diameter = s,
-            Message::RodDiameterChanged(s) => self.rod_diameter = s,
-            Message::AmplitudeChanged(s) => self.amplitude = s,
-            Message::FrequencyChanged(s) => self.frequency = s,
-            Message::CalcButtonPressed => (),
+            Message::PistonDiameterChanged(s) => {
+                if s.parse::<u32>().is_ok() || s.is_empty() {
+                    self.piston_diameter = s;
+                }
+            }
+
+            Message::RodDiameterChanged(s) => {
+                if s.parse::<u32>().is_ok() || s.is_empty() {
+                    self.rod_diameter = s;
+                }
+            }
+
+            Message::AmplitudeChanged(s) => {
+                if s.parse::<u32>().is_ok() || s.is_empty() {
+                    self.amplitude = s;
+                }
+            }
+
+            Message::FrequencyChanged(s) => {
+                if s.parse::<f32>().is_ok() || s.is_empty() {
+                    self.frequency = s;
+                }
+            }
+
+            Message::CalcButtonPressed => {
+                match try_calc_q(
+                    &self.piston_diameter,
+                    &self.rod_diameter,
+                    &self.amplitude,
+                    &self.frequency,
+                ) {
+                    Ok((main_result, secondary_result)) => {
+                        self.main_result = main_result.to_string();
+                        self.secondary_result = secondary_result.to_string();
+                        self.last_error = " ".to_string(); // to not hide error widget on gui
+                    }
+                    Err(e) => {
+                        self.last_error = e.to_string();
+                    }
+                }
+            }
+
             Message::DoNothing => (),
         }
     }
@@ -84,11 +149,12 @@ impl Sandbox for MiniApp {
         let piston_widget = TextInput::new(
             &mut self.piston_widget_state,
             "",
-            &self.piston_diameter,
+            &self.piston_diameter.to_string(),
             Message::PistonDiameterChanged,
         )
         .padding(5)
-        .width(Length::Units(100));
+        .width(Length::Units(100))
+        .on_submit(Message::CalcButtonPressed);
 
         let rod_widget = TextInput::new(
             &mut self.rod_widget_state,
@@ -97,7 +163,8 @@ impl Sandbox for MiniApp {
             Message::RodDiameterChanged,
         )
         .padding(5)
-        .width(Length::Units(100));
+        .width(Length::Units(100))
+        .on_submit(Message::CalcButtonPressed);
 
         let amplitude_widget = TextInput::new(
             &mut self.amplitude_widget_state,
@@ -106,7 +173,8 @@ impl Sandbox for MiniApp {
             Message::AmplitudeChanged,
         )
         .padding(5)
-        .width(Length::Units(100));
+        .width(Length::Units(100))
+        .on_submit(Message::CalcButtonPressed);
 
         let frequency_widget = TextInput::new(
             &mut self.frequency_widget_state,
@@ -115,7 +183,8 @@ impl Sandbox for MiniApp {
             Message::FrequencyChanged,
         )
         .padding(5)
-        .width(Length::Units(100));
+        .width(Length::Units(100))
+        .on_submit(Message::CalcButtonPressed);
 
         let calc_button_widget =
             button::Button::new(&mut self.calc_button_state, Text::new("Вычислить"))
@@ -123,14 +192,16 @@ impl Sandbox for MiniApp {
                 .style(MyButtonStyle)
                 .on_press(Message::CalcButtonPressed);
 
+        let error_message_widget = Text::new(&self.last_error).color([1.0, 0.0, 0.0]).size(17);
+
         let input_layout = Column::new()
             .spacing(10)
             .align_items(Align::End)
             .push(
                 Row::new()
-                .spacing(10)
-                .push(Text::new("Исходные данные").color(Color::from_rgb8(188, 195, 206)))
-                .push(Space::new(Length::Units(10), Length::Units(1)))
+                    .spacing(10)
+                    .push(Text::new("Исходные данные").color(Color::from_rgb8(188, 195, 206)))
+                    .push(Space::new(Length::Units(10), Length::Units(1))),
             )
             .push(Space::new(Length::Units(1), Length::Units(5)))
             .push(
@@ -173,11 +244,13 @@ impl Sandbox for MiniApp {
                     .align_items(Align::Center)
                     .push(calc_button_widget)
                     .push(Space::new(Length::Units(30), Length::Units(1))),
-            );
+            )
+            .push(Space::new(Length::Units(1), Length::Units(20)))
+            .push(error_message_widget);
 
         let main_result_widget = TextInput::new(
             &mut self.main_result_widget_state,
-            "result",
+            "результат",
             &self.main_result,
             // use as output only
             |_s| Message::DoNothing,
@@ -189,7 +262,7 @@ impl Sandbox for MiniApp {
 
         let secondary_result_widget = TextInput::new(
             &mut self.secondary_result_widget_state,
-            "result",
+            "результат",
             &self.secondary_result,
             // use as output only
             |_s| Message::DoNothing,
@@ -208,7 +281,8 @@ impl Sandbox for MiniApp {
             .push(main_result_widget)
             .push(Space::new(Length::Units(1), Length::Units(20)))
             .push(Text::new("Полученный расход Q[м^3/с]:").color(LIGHT_GRAY))
-            .push(secondary_result_widget);
+            .push(secondary_result_widget)
+            .push(Space::new(Length::Units(1), Length::Units(40)));
 
         let content = Row::new()
             .spacing(40)
@@ -264,7 +338,7 @@ impl text_input::StyleSheet for MyTextInputStyle {
         text_input::Style {
             background: Background::Color(Color::WHITE),
             border_radius: 0.0,
-            border_width: 0.0,
+            border_width: 1.0,
             border_color: Color::from_rgb(0.7, 0.7, 0.7),
         }
     }
@@ -288,7 +362,6 @@ impl text_input::StyleSheet for MyTextInputStyle {
         Color::from_rgb(0.8, 0.8, 1.0)
     }
 }
-
 
 fn main() {
     MiniApp::run(Settings::default());
